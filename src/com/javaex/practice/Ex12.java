46bfeea68c6e4ba5ee676aa5b64802a77013106f
gadongmin <dmkim777@naver.com>
2025-04-23 17:40:46 +0900

    20250423(2)

--- src/com/javaex/practice/Ex12.java
+++ src/com/javaex/practice/Ex12.java
@@ -12,7 +12,7 @@ public class Ex12 {
 		int num = sc.nextInt();
 		long result = 1;
 		for(int i=1; i<=num; i++) {//1초기화, 입력된 숫자 이하로 반복, 1씩증가
-			result = result*i; // 결과값이 1이면 숫다 4로 입력했을때, 4,3,2,1에 각 1씩 곱을 해준값이 결과로 나옴
+			result = result*i; // 결과값이 1이면 숫다 4로 입력했을때, 4,3,2,1에 각 1씩 곱을 해준값이 결과임
 		}
 		System.out.println("결과값: " + result);
 		
